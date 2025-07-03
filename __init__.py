80cd98c3786777cb3fa86d2dfad0411e7a741376
Ismael Marcos Martínez <ismael.marcos@nan-tic.com>
2025-07-03 12:43:58 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# This file is part aeat_rege module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import Pool
+
+def register():
+    Pool.register(
+        module='aeat_rege', type_='model')
+    Pool.register(
+        module='aeat_rege', type_='wizard')
+    Pool.register(
+        module='aeat_rege', type_='report')
