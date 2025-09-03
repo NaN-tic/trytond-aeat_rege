8a098b19e3744ffbcc0217006d353c209f4ca2ea
nan-tic-dev <79672209+nan-tic-dev@users.noreply.github.com>
2025-09-03 17:21:27 +0200

    Implement Special Business Group Regime (REGE)

--- __init__.py
+++ __init__.py
@@ -2,9 +2,15 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
+from . import rege
 
 def register():
     Pool.register(
+        rege.REGE,
+        rege.REGEPeriod,
+        rege.REGEMember,
+        rege.Party,
+        rege.InvoiceLine,
         module='aeat_rege', type_='model')
     Pool.register(
         module='aeat_rege', type_='wizard')
